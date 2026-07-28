ad6b66ccffad761dcd182e2a2205ab4a459a318f
EngReteti <amasonlerionka@gmail.com>
2026-07-29 00:52:57 +0300

    12. Array - Contains Duplicate

--- leetcode/array/P0217_ContainsDuplicate.java
+++ leetcode/array/P0217_ContainsDuplicate.java
@@ -1,24 +1,23 @@
 package leetcode.array;
 
 import java.util.HashSet;
+import java.util.Set;
 
 /**
- * 0002/1000 - Contains Duplicate
+ * 0012/1000 - Contains Duplicate
  * Difficulty: Easy
+ * Pattern: Hash Set Lookup
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  */
 public class P0217_ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        // A HashSet only stores unique elements
-        HashSet<Integer> seen = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
         
         for (int num : nums) {
-            // If the set already contains the number, it's a duplicate
             if (seen.contains(num)) {
                 return true;
             }
-            // Otherwise, add it to the set
             seen.add(num);
         }
         
@@ -27,11 +26,11 @@ public class P0217_ContainsDuplicate {
 
     public static void main(String[] args) {
         P0217_ContainsDuplicate solver = new P0217_ContainsDuplicate();
-        
+
         int[] test1 = {1, 2, 3, 1};
         int[] test2 = {1, 2, 3, 4};
-        
-        System.out.println("2. Array - Contains Duplicate (Test 1): " + solver.containsDuplicate(test1));
-        System.out.println("2. Array - Contains Duplicate (Test 2): " + solver.containsDuplicate(test2));
+
+        System.out.println("12. Array - Contains Duplicate Test 1: " + solver.containsDuplicate(test1));
+        System.out.println("12. Array - Contains Duplicate Test 2: " + solver.containsDuplicate(test2));
     }
 }
